5dce9ba1a0fb7a60e7b7761f6d48bc46263ec7fc
Andy Georges <itkovian@gmail.com>
2017-12-04 09:19:54 +0100

    2017: day 3 - Haskell

---
+++ 2017/advent4_b.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Control.Applicative
+import Data.List
+
+main = do
+
+    passwords <- (map (group . sort . map sort . words) . lines) <$> getContents
+
+    print $ length $ filter (all ((==) True) . map (((==) 1) . length))  passwords
