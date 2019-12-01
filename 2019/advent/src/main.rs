780190a4200f6bd6fb3a8b73f90a671104bf0a40
Andy Georges <itkovian@gmail.com>
2019-12-01 20:21:34 +0100

    Headers and version

--- 2019/advent/src/main.rs
+++ 2019/advent/src/main.rs
@@ -1,3 +1,24 @@
+/*
+Copyright 2019 Andy Georges <itkovian+sarchive@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 mod advent;
 
@@ -5,6 +26,7 @@ use advent::*;
 use clap::{App, Arg};
 use std::collections::HashMap;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 type Callback = fn() -> ();
 
 fn not_started() -> () {
@@ -14,7 +36,7 @@ fn not_started() -> () {
 fn main() {
 
     let matches = App::new("advent")
-        .version("2019.0.1")
+        .version(VERSION)
         .author("Andy Georges")
         .about("Advent of code solutions for 2019")
         .arg(
