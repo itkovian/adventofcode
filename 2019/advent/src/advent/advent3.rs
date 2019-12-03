9b5f8b2881cf9975c8723264acd74267bcd3efb0
Andy Georges <itkovian@gmail.com>
2019-12-03 23:56:14 +0100

    Code cleanup

--- 2019/advent/src/advent/advent3.rs
+++ 2019/advent/src/advent/advent3.rs
@@ -27,7 +27,8 @@ use std::io::BufReader;
 use std::collections::{HashMap, HashSet};
 
 type Pos = (i32, i32);
-type Info = (Pos, i32);
+type Count = i32;
+type Info = (Pos, Count);
 
 #[derive(Debug, PartialEq)]
 enum Direction {
@@ -50,27 +51,22 @@ impl Direction {
 
 }
 
-fn path(x: i32, y: i32, route: &Vec<(Direction, i32)>) -> Vec<Info> {
-    let mut p = vec![((x, y), 0)];
+fn path(route: &Vec<(Direction, i32)>) -> Vec<Info> {
+    let mut p = vec![(0, 0)];
 
     route.iter()
-        .fold(((x, y), 0), |((x_, y_), s_), (d, steps)| {
-            let ((mut x, mut y), mut s) = ((x_, y_), s_);
-            for _ in 0..*steps {
-                let (x__, y__) = d.make_move();
-                x = x + x__;
-                y = y + y__;
-                s += 1;
-                p.push(((x,y), s));
+        .fold(p[0], |(x, y), (d, steps)| {
+            let (mx, my) = d.make_move();
+            for i in 1..(*steps + 1) {
+                p.push((x + i * mx, y + i * my));
             };
-            ((x, y), s)
+            (x + steps * mx, y + steps * my)
         });
 
-    p
+    p.iter().cloned().zip((0 as i32)..).collect()
 }
 
 fn conversion(p: &str) -> (Direction, i32) {
-
     let d = match &p[0..1] {
         "L" => Direction::L,
         "U" => Direction::U,
@@ -82,32 +78,32 @@ fn conversion(p: &str) -> (Direction, i32) {
     (d, (p[1..]).parse::<i32>().unwrap())
 }
 
-fn distance(route1: &Vec<Info>, route2: &Vec<Info>) -> i32 {
-
+fn intersections(route1: &Vec<Info>, route2: &Vec<Info>) -> HashSet<Pos> {
     let rs1 : HashSet<Pos> = route1.iter().cloned().map(|i| i.0).collect();
     let rs2 : HashSet<Pos>  = route2.iter().cloned().map(|i| i.0).collect();
+    rs1.intersection(&rs2).cloned().collect()
+}
 
-    let mut distances : Vec<(i32, Pos)> = rs1.intersection(&rs2)
-        .map(|(x, y)| {
-            ((x-1).abs() + (y-1).abs(), (*x, *y))
-        }).collect();
+fn distance(route1: &Vec<Info>, route2: &Vec<Info>) -> i32 {
+
+    let is = intersections(route1, route2);
+    let mut distances : Vec<i32> = is.iter()
+        .map(|(x, y)| x.abs() + y.abs())
+        .collect();
 
     distances.sort();
-    distances[1].0
+    distances[1]
 }
 
 fn wire_length(route1: &Vec<Info>, route2: &Vec<Info>) -> i32 {
 
-    let rs1 : HashSet<Pos> = route1.iter().cloned().map(|i| i.0).collect();
-    let rs2 : HashSet<Pos>  = route2.iter().cloned().map(|i| i.0).collect();
-
-    let intersections : HashSet<&Pos> = rs1.intersection(&rs2).collect();
+    let is = intersections(route1, route2);
 
     let hm1 : HashMap<Pos, i32> = route1.iter().cloned().collect();
     let hm2 : HashMap<Pos, i32> = route2.iter().cloned().collect();
 
     let mut m = route1.last().unwrap().1 + route2.last().unwrap().1;
-    for pos in intersections.iter() {
+    for pos in is.iter() {
         let d1 = hm1.get(pos).unwrap();
         let d2 = hm2.get(pos).unwrap();
         if d1 + d2 > 0 { m = min(m, d1+d2); }
@@ -120,7 +116,7 @@ fn read(p: &mut BufReader<File>) -> Vec<Vec<Info>> {
 
     p.lines()
         .map(|l| l.unwrap().trim().split(",").map(|r| conversion(&r)).collect::<Vec<(Direction, i32)>>())
-        .map(|r| path(1, 1, &r))
+        .map(|r| path(&r))
         .collect::<Vec<Vec<Info>>>()
 
 }
@@ -128,7 +124,6 @@ fn read(p: &mut BufReader<File>) -> Vec<Vec<Info>> {
 pub fn advent3a(p: &mut BufReader<File>) -> () {
 
     let routes = read(p);
-
     let distance = distance(&routes[0], &routes[1]);
     println!("Closest intersection is {:?}", distance);
 
@@ -138,9 +133,7 @@ pub fn advent3a(p: &mut BufReader<File>) -> () {
 pub fn advent3b(p: &mut BufReader<File>) -> () {
 
     let routes = read(p);
-
     let length = wire_length(&routes[0], &routes[1]);
-
     println!("Minimal wire length: {}", length);
 
 }
@@ -166,17 +159,17 @@ mod tests {
             .map(|r| conversion(&r))
             .collect::<Vec<(Direction, i32)>>();
 
-        assert_eq!(path(1, 1, &route1), vec![((1,1), 0), ((2,1), 1), ((3,1), 2), ((3,2), 3), ((3,3), 4), ((2,3), 5), ((1,3), 6), ((1,2), 7), ((1,1), 8)]);
+        assert_eq!(path(&route1), vec![((0, 0), 0), ((1,0), 1), ((2,0), 2), ((2,1), 3), ((2, 2), 4), ((1, 2), 5), ((0, 2), 6), ((0, 1), 7), ((0, 0), 8)]);
     }
 
     #[test]
     fn test_distance() {
 
-        let r1 = path(1, 1, &"R75,D30,R83,U83,L12,D49,R71,U7,L72"
+        let r1 = path(&"R75,D30,R83,U83,L12,D49,R71,U7,L72"
             .split(",")
             .map(|r| conversion(&r))
             .collect::<Vec<(Direction, i32)>>());
-        let r2 = path(1, 1, &"U62,R66,U55,R34,D71,R55,D58,R83"
+        let r2 = path(&"U62,R66,U55,R34,D71,R55,D58,R83"
             .split(",")
             .map(|r| conversion(&r))
             .collect::<Vec<(Direction, i32)>>());
