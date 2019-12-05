eb343bf49ecd12eaa7c0bf69d01b6dadc784fadb
Andy Georges <itkovian@gmail.com>
2019-12-05 22:34:57 +0100

    Day  5 assignment basic solution

--- 2019/advent/src/main.rs
+++ 2019/advent/src/main.rs
@@ -19,11 +19,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 mod advent;
 
 use advent::*;
+use chrono;
 use clap::{App, Arg};
+use log;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
@@ -34,6 +35,22 @@ type Callback = fn(&mut BufReader<File>) -> ();
 
 fn not_started(_bf: &mut BufReader<File>) -> () {
     println!("Please solve something.");
+}
+
+fn setup_logging(level: log::LevelFilter) -> Result<(), log::SetLoggerError> {
+    let base_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}][{}] {}",
+                chrono::Local::now().to_rfc3339(),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(level);
+
+    base_config.chain(std::io::stdout()).apply()
 }
 
 fn main() {
@@ -56,7 +73,15 @@ fn main() {
                 .default_value("input")
                 .help("Location of the input files")
         )
+        .arg(
+            Arg::with_name("debug")
+                .long("debug")
+                .help("Output debug information. Default log level is WARN")
+        )
         .get_matches();
+
+    let loglevel = if matches.is_present("debug") { log::LevelFilter::Debug } else { log::LevelFilter::Warn };
+    setup_logging(loglevel).expect("Cannot set up logging");
 
     let mut advent_map : HashMap<&str, Callback> = HashMap::new();
     let advent_ex = matches.value_of("day").unwrap();
@@ -77,6 +102,8 @@ fn main() {
     advent_map.insert("3b", advent3::advent3b);
     advent_map.insert("4a", advent4::advent4a);
     advent_map.insert("4b", advent4::advent4b);
+    advent_map.insert("5a", advent5::advent5a);
+    advent_map.insert("5b", advent5::advent5b);
 
     println!("Hello, advent of code!");
     match advent_map.get(advent_ex) {
